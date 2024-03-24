48b448f2cb1fedce8136e90741e01b612831bd6a
Md Mijanur Rahman <md.rahman@fau.de>
2024-03-24 23:51:51 +0100

    Add train and test data loading

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import numpy as np
 @hydra.main(version_base='1.3.2', config_path="configs", config_name="main_config.yaml")    
 def main(cfg: DictConfig) -> None:
     dataset = hydra.utils.instantiate(cfg.datasets.model_params)
+
+    # load train and test data
+    X_train, y_train, X_test, y_test = dataset.train_test_split(
+        df=dataset.process_data(),
+        **cfg.datasets.data_params
+    )
+    return
     
 if __name__ == "__main__":  
     main()
