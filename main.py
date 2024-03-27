02d03fef20ae68ee9a50b4e003a8b0a38e2d7d32
Md Mijanur Rahman <md.rahman@fau.de>
2024-03-27 12:38:14 +0100

    Add XGBoost model training and evaluation

--- main.py
+++ main.py
@@ -54,6 +54,37 @@ def main(cfg: DictConfig) -> None:
             df=dataset.process_data(),
             **cfg.datasets.data_params
         )
+        if cfg.models.arch.model_name == 'XGB':
+            model = instantiate(cfg.models.model)
+
+            # params
+            params = {
+                'X_train': X_train,
+                'y_train': y_train,
+                'arch': cfg.models.arch.model_name
+            }
+
+            # train the model
+            model.train_model(**params)
+
+            # evaluate the model
+            eval_params = {
+                'arch': cfg.models.arch.model_name,
+                'X_test': X_test,
+                'y_test': y_test
+            }
+            outputDict = model.evaluate_model(**eval_params)
+
+            # get fig
+            fig = model.plot_predictions(cfg.models.arch.model_name, outputDict['target'], outputDict['preds'])
+            outputDict['fig'] = fig
+
+            # log results to mlflow
+            log_results(cfg, outputDict)
+
+        else:
+            return
+
     return
     
 if __name__ == "__main__":  
